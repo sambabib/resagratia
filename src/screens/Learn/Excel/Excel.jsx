232f7b47ba19b2a54364f780c76a929927dd574b
Emmanuel Akala <adekyte@gmail.com>
2022-01-11 01:22:33 +0100

    fixed form placeholder on mobile v1

--- src/screens/Learn/Excel/Excel.jsx
+++ src/screens/Learn/Excel/Excel.jsx
@@ -5,27 +5,27 @@ import ExcelLogo from '../../../assets/excel.png';
 import './excel.scss';
 
 const tags = [
-    {
-        id: 1,
-        name: "tag__courses",
-        data: "3 courses"
-    },
-    {
-        id: 2,
-        name: "tag__hours",
-        data: "10 hours"
-    },
-    {
-        id: 3,
-        name: "tag__projects",
-        data: "5 projects"
-    },
-    {
-        id: 4,
-        name: "tag__resources",
-        data: "11 resources"
-    },
-]
+  {
+    id: 1,
+    name: 'tag__courses',
+    data: '3 courses',
+  },
+  {
+    id: 2,
+    name: 'tag__hours',
+    data: '10 hours',
+  },
+  {
+    id: 3,
+    name: 'tag__projects',
+    data: '5 projects',
+  },
+  {
+    id: 4,
+    name: 'tag__resources',
+    data: '11 resources',
+  },
+];
 
 const Excel = () => {
   return (
@@ -52,14 +52,40 @@ const Excel = () => {
 
             {/* title tags */}
             <div className='title__tags'>
-                {tags.map(tag => (
-                    <div className={tag.name}>
-                        <p>{tag.data}</p>
-                    </div>
-                ))}
+              {tags.map((tag) => (
+                <div className={tag.name}>
+                  <p>{tag.data}</p>
+                </div>
+              ))}
             </div>
           </div>
-          <div className='title__media'></div>
+          <div className='title__media'>
+            <div className='title__media__container'>
+              <div className='media__container'>
+                <video
+                  src='https://youtu.be/ysTcxiyAjYw'
+                  controls='controls'
+                  autoplay='true'
+                ></video>
+                <div className='media__getstarted__button'>
+                  <p>Get started</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* content */}
+        <div className="content__and__tutors">
+            <div className="content__container">
+                <div className="content__items">
+                    <div className="content__left">
+                        <div className="circle"></div>
+                        <div className="border"></div>
+                    </div>
+                    <div className="content__right"></div>
+                </div>
+            </div>
         </div>
       </div>
     </div>
