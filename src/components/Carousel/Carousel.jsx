2d16bf7f0c85f9ac2441fdf26245b53321da1379
Emmanuel Akala <adekyte@gmail.com>
2022-01-25 16:04:26 +0100

    bug fix mobile navbar

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -62,6 +62,26 @@ const Carousel = () => {
       user: 'Oluwatobi',
       details: `The tutor's approach to teaching is top-notch and 100% practical. I would recommend this course to Excel users of any level of proficiency. This course arguably ranks among my top 3 online courses ever.`,
     },
+    {
+      id: 7,
+      user: 'Jinesh',
+      details: `Explained in the most simple language.`,
+    },
+    {
+      id: 8,
+      user: 'Catherine',
+      details: `The pace of this one is good, given there is an expectation of basic Excel knowledge. I'm learning more useful tips and tricks more quickly than I expected. Thank you!`,
+    },
+    {
+      id: 9,
+      user: 'Tony',
+      details: `It was a packed course, I learned so much in a short time. I never knew excel had so many capabilities. Instructor was very knowledgeable in this area.`,
+    },
+    {
+      id: 10,
+      user: 'Elizabeth',
+      details: `It was very good for me. Amazing!!!`,
+    },
   ];
 
   return (
